728b518eadcf50960d1b4c9acff84b98f8b487e1
mercy <mcherono529@gmail.com>
2025-02-25 01:45:36 +0300

    order table

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from models import db
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shopIT.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+api = Api(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
