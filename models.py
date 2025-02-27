953e8ef4f6ce767267332884b2a460db2a60202e
Racheal <rachealnjugush@gmail.com>
2025-02-27 21:45:51 +0300

    change title column to name

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     seller_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    title = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
