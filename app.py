9a2f8ca2b941bbdf181bc775d54988f4274dbf7e
Racheal Wambui Njuguna <rachealnjugush@gmail.com>
2025-03-10 10:06:41 +0300

    create env to deploy

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
 from models import db, OrderItem, Cart, User, Product
 from resources.shop import UserResource ,RegisterResource, LoginResource, ProductDisplayResource, ProductResource, OrderDisplayResource, OrderResource
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shopit.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'bc9f3a4897ef90fc6c27f17ee1905a2f'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)
